a0b792383452751841a4cc82735268845ead0e9b
LouisTruch <louis.truchelut@gmail.com>
2024-04-02 09:39:35 +0200

    Style for input

--- src/atom/input/style.rs
+++ src/atom/input/style.rs
@@ -7,12 +7,17 @@ impl std::fmt::Display for BaseClass<InputProps> {
             BaseClass::BaseClass => {
                 "
                 flex w-full \
-                text-sm text-foreground \
-                bg-input focus:brightness-105 hover:brightness-105 \
+                h-10 \
                 px-3 py-2 \
+                text-sm text-foreground \
+                bg-background \
                 border border-input \
                 rounded-global-radius \
-                disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:brightness-100"
+                hover:brightness-105 \
+                focus:outline-none \
+                focus:brightness-105 focus:ring-ring focus:ring-offset-2 focus:ring-2 \
+                disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:brightness-100 \
+                file:font-medium file:bg-input file:rounded-global-radius file:border-0"
             }
             _ => "",
         };
