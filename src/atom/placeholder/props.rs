456b2b0deacea49ee758ff067aaf0b1dbc0b3d24
LouisTruch <louis.truchelut@gmail.com>
2024-04-16 14:30:08 +0200

    Added width and height for PlaceHolder Atom

--- src/atom/placeholder/props.rs
+++ src/atom/placeholder/props.rs
@@ -5,8 +5,18 @@ use tailwind_fuse::*;
 use crate::types::*;
 
 #[props_component(id, class)]
-pub fn Placeholder(#[props(default)] animation: super::PlaceholderAnimation) -> Element {
-    let class = tw_merge!(props.base(), props.variant(), props.class);
+pub fn Placeholder(
+    #[props(default)] animation: super::PlaceholderAnimation,
+    #[props(default = 24)] width: u32,
+    #[props(default = 4)] height: u32,
+) -> Element {
+    let class = tw_merge!(
+        props.base(),
+        props.variant(),
+        format!("w-{}", props.width),
+        format!("h-{}", props.height),
+        props.class
+    );
 
-    rsx!( div { class: class } )
+    rsx!(div { class: class })
 }
--- src/website/pages/components/atoms/placeholder_page.rs
+++ src/website/pages/components/atoms/placeholder_page.rs
@@ -4,7 +4,7 @@ use dioxus_components_bin::atom::placeholder::*;
 pub fn PlaceholderPage() -> Element {
     rsx!(
         "PLACEHOLDER PAGE"
-        Placeholder { class: "rounded-global-radius w-20 h-20", animation: PlaceholderAnimation::None }
+        Placeholder { class: "rounded-global-radius", width: 40, animation: PlaceholderAnimation::None }
         Placeholder { class: "rounded-full w-40 h-40", animation: PlaceholderAnimation::Light }
         Placeholder { class: "rounded-global-radius w-80 h-4", animation: PlaceholderAnimation::Full }
         "No animation"
