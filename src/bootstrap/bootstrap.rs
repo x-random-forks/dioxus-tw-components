ab7effe85056e6f15958721864833005940387e2
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-02-27 12:27:04 +0100

    moving docsite to it's own repo, improving readme

--- src/bootstrap/bootstrap.rs
+++ src/bootstrap/bootstrap.rs
@@ -1,12 +1,17 @@
 use dioxus::prelude::*;
 
-#[derive(PartialEq, Clone, Default)]
+#[derive(Default, Clone, PartialEq, Props)]
 pub struct BootstrapConfig {
+    #[props(optional, default)]
+    icon: Option<Asset>,
 }
 
 #[component]
-pub fn DioxusTwComponentsBootstrap(config: BootstrapConfig) -> Element {
+pub fn DioxusTwComponentsBootstrap(props: BootstrapConfig) -> Element {
     rsx! {
         document::Stylesheet { href: "https://fonts.googleapis.com/icon?family=Material+Icons" }
+        if let Some(icon) = props.icon {
+            document::Link { rel: "icon", href: icon }
+        }
     }
 }
